406fde4ff3f130656e9ac9ddab1ab8369faaba85
jefferson <jeffersonteixeira@outlook.com>
2021-12-16 12:22:18 -0300

    simple exceptions catch

--- src/main/java/com/example/surittec/desafio/controller/ClientController.java
+++ src/main/java/com/example/surittec/desafio/controller/ClientController.java
@@ -1,8 +1,10 @@
 package com.example.surittec.desafio.controller;
 
 import com.example.surittec.desafio.domain.Client;
+import com.example.surittec.desafio.repository.ClientRepository;
 import com.example.surittec.desafio.service.ClientService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.modelmapper.spi.ErrorMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.util.NoSuchElementException;
 
@@ -36,26 +39,41 @@ public class ClientController {
 
     @PostMapping
     @PreAuthorize("hasAuthority('ADMIN')")
-    public ResponseEntity<?> saveClient(@Valid @RequestBody Client client) throws JsonProcessingException { //@TODO HANDLE EXCEPTION
-
-        if (client.getId() == null) {
-            return new ResponseEntity<>(clientService.save(client), HttpStatus.CREATED);
-        } else {
-            return ResponseEntity.ok(clientService.save(client));
+    public ResponseEntity<?> saveClient(@Valid @RequestBody Client client) {
+        try {
+            if (client.getId() == null) {
+                return new ResponseEntity<>(clientService.save(client), HttpStatus.CREATED);
+            } else {
+                return ResponseEntity.ok(clientService.save(client));
+            }
+        } catch (EntityNotFoundException | NoSuchElementException ex) {
+            return new ResponseEntity<>(new ErrorMessage("Não encontrado cliente com id:" + client.getId()), HttpStatus.NOT_FOUND);
+        } catch (JsonProcessingException ex) {
+            return ResponseEntity.internalServerError().build();
         }
     }
 
     @GetMapping("/{id}")
     @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
     public ResponseEntity<?> getClient(@PathVariable Long id) {
-        return ResponseEntity.ok(clientService.getClientById(id).orElseThrow(NoSuchElementException::new));
+        try {
+            return ResponseEntity.ok(clientService.getClientById(id).orElseThrow(NoSuchElementException::new));
+        } catch (EntityNotFoundException | NoSuchElementException ex) {
+            return new ResponseEntity<>(new ErrorMessage("Não encontrado cliente com id:" + id), HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/{id}")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public ResponseEntity<?> removeClient(@PathVariable Long id) throws JsonProcessingException {
-        clientService.removeById(id);
-        return ResponseEntity.ok("");
+    public ResponseEntity<?> removeClient(@PathVariable Long id) {
+        try {
+            clientService.removeById(id);
+            return ResponseEntity.ok("");
+        } catch (EntityNotFoundException | NoSuchElementException ex) {
+            return new ResponseEntity<>(new ErrorMessage("Não encontrado cliente com id:" + id), HttpStatus.NOT_FOUND);
+        } catch (JsonProcessingException ex) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
 }
